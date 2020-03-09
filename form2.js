2016c7e8785dc78e05e0fc82725f0a1847e9ffdd
Monishwaran v <monishwaranvenkat@gmail.com>
2020-03-09 12:10:21 +0530

    d

--- form2.js
+++ form2.js
@@ -1,6 +1,8 @@
 var error;
 var name_regx="^[a-b A-z]{2,35}$";
 var email_regx="^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$";
+var user_regx="^(?!\\d+$)\\w{8,20}$"
+var pass_regx="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
 function validate(){
     if (vfname())
     {
@@ -11,6 +13,18 @@ function validate(){
     }if(vemail()){
         return false
     }
+    if(vuserid())
+    {
+        return false
+    }
+    if(vpass()){
+        return false
+    }
+    if(vcnfpass()){
+        return false
+    }
+    $( "form" ).submit();
+    alert("Registeration Success");
 }
 function vfname(){
     if ($("#fname").val()==""|| !$("#fname").val().match(name_regx))
@@ -38,6 +52,19 @@ function vlname(){
         return false;
     }
 }
+function vuserid(){
+    if ($("#userid").val()==""|| !$("#userid").val().match(user_regx))
+    {
+        document.getElementById("userid").style.borderColor="red";
+        document.getElementById("userid-warn").innerText="Enter the valid user id";
+        return true;
+        
+    }else{
+        document.getElementById("userid").style.borderColor="green";
+        document.getElementById("userid").innerText="valid";
+        return false;
+    }
+}
 function vemail(){
     if ($("#email").val()==""|| !$("#email").val().match(email_regx))
     {
@@ -51,4 +78,31 @@ function vemail(){
         return false;
     }
 }
+function vpass(){
+    if ($("#pass").val()==""|| !$("#pass").val().match(pass_regx))
+    {
+        document.getElementById("pass").style.borderColor="red";
+        document.getElementById("pass-warn").innerText="Enter the valid password";
+        return true;
+        
+    }else{
+        document.getElementById("pass").style.borderColor="green";
+        document.getElementById("pass-warn").innerText="valid";
+        return false;
+    }
+}
+function vcnfpass(){
+    if ($("#cnf-pass").val()=="" || $("#pass").val()!= $("#cnf-pass").val() || !$("#cnf-pass").val().match(pass_regx))
+    {
+        document.getElementById("cnf-pass").style.borderColor="red";
+        document.getElementById("cnf-pass-warn").innerText="Password did not match";
+        return true;
+        
+    }else{
+        document.getElementById("cnf-pass").style.borderColor="green";
+        document.getElementById("cnf-pass-warn").innerText="valid";
+        return false;
+    }
+}
+
 
